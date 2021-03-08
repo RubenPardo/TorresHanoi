fd92c1c8757ec06f8caca1138921a71b510c638b
Ruben <rubenpardocasanova@gmail.com>
2021-03-08 18:31:03 +0100

    primera version clase pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -8,35 +8,61 @@ namespace Torres_de_Hanoi
 {
     class Pila
     {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+
+
+        private List<Disco> elementos;
+        private int top;// -1 si no hay ninguna
+
+        // numero de discos en la pila
+        public int Size {
+            get {
+                return elementos.Count();
+            }
+        }
+        // disco en el top de la pila, se indica el tamaño
+        public int Top { 
+            get { return top; } 
+            set { top = value; }
+        }    
+       
+        public List<Disco> Elementos { 
+            get{
+                return elementos;
+            }
+        }
+       
 
         /* TODO: Implementar métodos */
         public Pila()
         {
-
+            elementos = new List<Disco>();
         }
 
         public void push(Disco d)
         {
-
+            top = d.Value();
+            elementos.Add(d);
         }
 
         public Disco pop()
         {
-            return null;
+            Disco discoExtraido = elementos.Last();
+            elementos.Remove(discoExtraido);
+            // si al borrar el disco esta vacio el top = -1
+            if (!isEmpty())
+            {
+                top = elementos.Last().Value();
+            }
+            else
+            {
+                top = -1;
+            }
+            return discoExtraido;
         }                
 
         public bool isEmpty()
         {
-            return true;
+            return elementos.Count() == 0;
         }
 
     }

f4c59762cbd36559333fb393f831a5b150391580
Ruben <rubenpardocasanova@gmail.com>
2021-03-08 19:59:59 +0100

    merge on master

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,16 +8,184 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
-        /*TODO: Implementar métodos*/
+        
         public void mover_disco(Pila a, Pila b)
         {
 
+            if (a.isEmpty() && !b.isEmpty())
+            {
+                // mover de B a A
+                Disco dB = b.pop();//quitar de B
+                a.push(dB);// poner en A
+                Console.WriteLine("Se mueve: " + dB.Valor + " de " + b.Nombre + " a " + a.Nombre);
+            
+            }
+            else if(!a.isEmpty() && b.isEmpty())
+            {
+                // mover de A a B
+                Disco dA = a.pop();//quitar de A
+                b.push(dA);// poner en B
+                Console.WriteLine("Se mueve: " + dA.Valor + " de " + a.Nombre + " a " + b.Nombre);
+            }
+            else
+            {
+                int topA = a.Top;
+                int topB = b.Top;
+
+                if (topA > topB)
+                {
+                    // si a tiene una ficha mas grande se tendrá que mover de B a A
+                    // mover de B a A
+                    Disco dB = b.pop();//quitar de B
+                    a.push(dB);// poner en A
+                    Console.WriteLine("Se mueve: " + dB.Valor + " de " + b.Nombre + " a " + a.Nombre);
+                }
+                else
+                {
+                    // mover de A a B
+                    Disco dA = a.pop();//quitar de A
+                    b.push(dA);// poner en B
+                    Console.WriteLine("Se mueve: " + dA.Valor + " de " + a.Nombre + " a " + b.Nombre);
+                }
+
+            }
         }
 
+        /*
+         * n = numero de discos
+         * */
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
-            return 0;
+            int movimientos = 0;
+            bool haySolucion = false;
+            // si n es impar
+            if(n%2 == 1)
+            {
+                while (!haySolucion)
+                {
+                    mover_disco(ini, fin);
+                    movimientos++;
+                    haySolucion = comprobar_victoria(n, fin);
+
+                    Console.WriteLine(ini.ToString());
+                    Console.WriteLine(aux.ToString());
+                    Console.WriteLine(fin.ToString());
+                    if (haySolucion) { break; }
+
+                    mover_disco(ini, aux);
+                    movimientos++;
+                    haySolucion = comprobar_victoria(n, fin);
+
+                    Console.WriteLine(ini.ToString());
+                    Console.WriteLine(aux.ToString());
+                    Console.WriteLine(fin.ToString());
+                    if (haySolucion) { break; }
+
+                    mover_disco(aux, fin);
+                    movimientos++;
+                    haySolucion = comprobar_victoria(n, fin);
+
+                    Console.WriteLine(ini.ToString());
+                    Console.WriteLine(aux.ToString());
+                    Console.WriteLine(fin.ToString());
+                    if (haySolucion) { break; }
+
+
+                }
+                
+            }
+            // si n es par
+            else
+            {
+                while (!haySolucion)
+                {
+                    mover_disco(ini, aux);
+                    movimientos++;
+                    haySolucion = comprobar_victoria(n, fin);
+
+                    Console.WriteLine(ini.ToString());
+                    Console.WriteLine(aux.ToString());
+                    Console.WriteLine(fin.ToString());
+                    if (haySolucion) { break; }
+
+                    mover_disco(ini, fin);
+                    movimientos++;
+                    haySolucion = comprobar_victoria(n, fin);
+
+                    Console.WriteLine(ini.ToString());
+                    Console.WriteLine(aux.ToString());
+                    Console.WriteLine(fin.ToString());
+                    if (haySolucion) { break; }
+
+                    mover_disco(aux, fin);
+                    movimientos++;
+                    haySolucion = comprobar_victoria(n, fin);
+
+                    Console.WriteLine(ini.ToString());
+                    Console.WriteLine(aux.ToString());
+                    Console.WriteLine(fin.ToString());
+                    if (haySolucion) { break; }
+
+
+                }
+            }
+
+
+            return movimientos;
         }
 
+
+        public int recursivo(int n, Pila ini, Pila fin, Pila aux)
+        {
+            int movimientos = 0;
+            if (n == 1)
+            {
+                mover_disco(ini, fin);
+                movimientos++;
+                Console.WriteLine(ini.ToString());
+                Console.WriteLine(aux.ToString());
+                Console.WriteLine(fin.ToString());
+            }
+            else
+            {
+                recursivo(n - 1, ini, aux, fin);
+                mover_disco(ini, fin);
+                movimientos++;
+                Console.WriteLine(ini.ToString());
+                Console.WriteLine(aux.ToString());
+                Console.WriteLine(fin.ToString());
+
+                recursivo(n - 1, aux, fin, ini);
+            }
+            
+
+            return movimientos;
+        }
+
+        /**
+         * Comprueba si la pila fin tiene todas las piezas y ademas 
+         * todas estan ordenadas
+         */
+        private bool comprobar_victoria(int n,Pila fin)
+        {
+            // si no tiene todas las piezas devolvemos false
+            if(fin.Size != n)
+            {
+                return false;
+            }
+
+            bool ordenada = true;
+     
+            // orden correcto de n,...,3,2,1
+            for(int i= fin.Elementos.Count; i<1;i++)
+            {
+                if (fin.Elementos[i].Valor > fin.Elementos[i - 1].Valor)
+                {
+                    ordenada = false;
+                    break;
+                }
+            }
+            return ordenada;
+        }
     }
 }
--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,10 +10,48 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
+            bool seguir = true;
 
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+             Console.WriteLine("EJERCICIO TORRES HANOI-------------------------------");
+
+            while (seguir)
+            {
+                // leer el numero n de piezas
+                Console.WriteLine("Introduzca numero de piezas: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                // crear las 3 pilas y el objeto hanoi
+                Pila p1 = new Pila("INI");
+                Pila p2 = new Pila("FIN");
+                Pila p3 = new Pila("AUX");
+
+                Hanoi h = new Hanoi();
+
+                // añadir los n discos a la pila ini
+                for (int i = n; i >= 1; i--)
+                {
+                    p1.push(new Disco(i));
+                }
+
+                Console.WriteLine(p1.ToString());
+                Console.WriteLine(p3.ToString());
+                Console.WriteLine(p2.ToString());
+
+                Console.WriteLine("Se completo en: " + h.iterativo(p1.Elementos.Count, p1, p2, p3) + " movimientos");
+
+                //Console.WriteLine("Se completo en: " + h.recursivo(p1.Elementos.Count, p1, p2, p3) + " movimientos");
+
+                Console.WriteLine("El número mínimo de movimientos para " + n + " piezas es: " + (Math.Pow(2, n) - 1));
+
+                Console.WriteLine("Pulsa 'q' para salir o cualquier otra para repetir");
+                char c = Console.ReadLine()[0];
+                if (c.Equals('q'))
+                {
+                    seguir = false;
+                }
+            }
+
+           
         }
     }
 }

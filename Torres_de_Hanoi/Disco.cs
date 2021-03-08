2357e5f79704ec72b3488c7448cc134c380408f3
Ruben <rubenpardocasanova@gmail.com>
2021-03-08 18:16:43 +0100

    Clase Disco
    Creado valor como numerico y constructor

--- Torres_de_Hanoi/Disco.cs
+++ Torres_de_Hanoi/Disco.cs
@@ -8,11 +8,15 @@ namespace Torres_de_Hanoi
 {
     class Disco
     {
-        /*TODO: 
-        Decidir tipo de Valor
+      
+        // a mayor numero mayor tamaño
         public int Valor { get; set; }
-        public String Valor { get; set; }
-        */
+
+        public Disco(int valor)
+        {
+            Valor = valor;
+        }
+        
 
     }
 }
